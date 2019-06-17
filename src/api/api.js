06ba118014dace6b80ab7ac0d2f9f3828d015101
ctf0106 <ctf0106@163.com>
2019-06-18 00:03:53 +0800

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,9 @@
 import { fetch } from "../service/services.js";
 
+
+//登陆接口
+export const login = data =>fetch("post", "", "/admin/login/login", data);
+
 //查询文章列表
 export const getArticleList = data =>fetch("post", "", "/admin/article/getArticleList", data);
 //查询类目列表
@@ -13,7 +17,12 @@ export const getArticleDetailById=data=>fetch("post","","/admin/article/getArtic
 //查询类目列表
 export const getCategoryAllList = data =>fetch("post", "", "/admin/category/getCategoryAllList", data);
 
-
 //保存或修改文章
 export const saveOrUpdateArticle = data =>fetch("post", "", "/admin/article/saveOrUpdateArticle", data);
+
+//超链接查询
+export const getLinksListPageInfo = data =>fetch("post", "", "/admin/links/getLinksListPageInfo", data);
+
+//删除超链接
+export const deleteLinksById = data =>fetch("post", "", "/admin/links/deleteLinksById", data);
 
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,38 +1,36 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="login-container">
+  <el-form :model="form" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="login-container">
     <h3 class="title">内容管理系统</h3>
     <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+      <el-input type="text" v-model="form.account" auto-complete="off" placeholder="账号"></el-input>
     </el-form-item>
     <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      <el-input type="password" v-model="form.checkPass" auto-complete="off" placeholder="密码"></el-input>
     </el-form-item>
     <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
       <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
-      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+      <!-- <el-button @click.native.prevent="handleReset2">重置</el-button> -->
     </el-form-item>
   </el-form>
 </template>
 
 <script>
-  // import { requestLogin } from '../api/api';
+  import { login } from '@/api/api';
   export default {
     data() {
       return {
         logining: false,
-        ruleForm2: {
+        form: {
           account: 'admin',
           checkPass: '123456'
         },
         rules2: {
           account: [
             { required: true, message: '请输入账号', trigger: 'blur' },
-            //{ validator: validaePass }
           ],
           checkPass: [
             { required: true, message: '请输入密码', trigger: 'blur' },
-            //{ validator: validaePass2 }
           ]
         },
         checked: true
@@ -48,7 +46,7 @@
           if (valid) {
             this.logining = true;
             var loginParams = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
-            requestLogin(loginParams).then(data => {
+            login(loginParams).then(data => {
               this.logining = false;
               let { msg, code, user } = data;
               if (code !== 200) {
